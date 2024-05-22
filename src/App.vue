6df87e64030194d7c99dd280d86c0f2da9bec3bc
jkkoh <jeongkyu7@gmail.com>
2024-05-22 20:18:51 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref } from 'vue'
+import BlogPost from './components/BlogPost.vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const posts = ref([
+  {id:1 , title:'처음', content:'예시'}
+])
+const vTitle = ref("")
+const vContent = ref("")
+const addPost = ()=>{
+  posts.value.push({
+    id: posts.value.length + 1,
+    title: vTitle.value,
+    content: vContent.value
+  })
+  vTitle.value = ''
+  vContent.value = ''
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div>
+    <h4>블로그 글 만들기</h4>
+    <p>제목</p>
+    <input v-model="vTitle" type="text">
+    <br/>
+    <p>내용</p>
+    <input v-model="vContent" type="text">
+    <br/>
+    <button @click="addPost">새로운 글 만들기</button>
+    <br/>
+    <BlogPost 
+    v-for="post in posts" 
+    :key="post.id" 
+    :id="post.id"
+    :title="post.title"
+    :content="post.content"
+    />
+  </div>
+</template>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+    props: {id:Number,
+            title:String, 
+            content:String}
+}
+</script>
+
+<template>
+    <div class="blogOuter">
+        <p>{{ id }} 번째 포스트</p>
+        <h4>제목 : {{ title }}</h4>
+        <p>내용 : {{ content }}</p>
+    </div>
+</template>
+
+<style>
+    .blogOuter{
+        margin-left: 2vw;
+        margin-right: 2vw;
+        padding-left: 10px;
+        padding-right: 10px;
+        padding-bottom: 2px;
+        box-sizing: border-box;
+        border-top: 1px solid black;
+    }
+</style>
